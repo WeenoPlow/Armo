d6ea3befff28865f8528cb0566acf199da36db45
WeenoPlow <m.durbet@gmail.com>
2015-09-05 00:19:05 +0200

    05-09-2015

---
+++ Ancien2/js/contactForm.js
@@ -0,0 +1,46 @@
+$(function(){
+            $("#contact").submit(function(event){
+                var nom        = $("#nom").val();
+                var sujet      = $("#objet").val();
+                var email      = $("#mail").val();
+                var message    = $("#mpBox").val();
+                var dataString = nom + sujet + email + message;
+                var msg_all    = "Merci de remplir tous les champs";
+                var msg_alert  = "Merci de remplir ce champs";
+
+                if (dataString  == "") {
+                    $("#msg_all").html(msg_all);
+                } else if (nom == "") {
+                    $("#msg_nom").html(msg_alert);
+                } else if (sujet == "") {
+                    $("#msg_objet").html(msg_alert);
+                } else if (email == "") {
+                    $("#msg_mail").html(msg_alert);
+                } else if (message == "") {
+                    $("#msg_mpBox").html(msg_alert);
+                } else {
+                    $.ajax({
+                        type : "POST",
+                        url: $(this).attr("action"),
+                        data: $(this).serialize(),
+                        success : function() {
+                            $("#validation").html("<p>Formulaire bien envoyé</p>");
+                        },
+                        error: function() {
+                            $("#validation").html("<p>Erreur d'appel, le formulaire ne peut pas fonctionner</p>");
+                        }
+                    });
+                }
+
+                return false;
+            });
+
+
+            function effacer () {
+              $('#contact',':input')
+               .not(':button, :submit, :reset, :hidden')
+               .val('')
+               .removeAttr('checked')
+               .removeAttr('selected');
+            }
+        });
